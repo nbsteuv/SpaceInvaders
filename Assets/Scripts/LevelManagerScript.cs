269932b312a8d001b0d69439e10ad2c5d572d66b
nbsteuv <nbsteuv@gmail.com>
2017-05-13 10:17:35 -0400

    Set up level flow with win condition cheat

--- Assets/Scripts/LevelManagerScript.cs
+++ Assets/Scripts/LevelManagerScript.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManagerScript : MonoBehaviour {
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            LoadNextLevel();
+        }
+    }
+
     public void StartGame()
     {
-        Debug.Log("Start");
+        SceneManager.LoadScene(1);
+    }
+
+    void LoadNextLevel()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index + 1);
     }
 }
