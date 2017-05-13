8c4ee49ea0ce1efd7878a18b28305dbebf99a1f5
nbsteuv <nbsteuv@gmail.com>
2017-05-13 12:56:44 -0400

    Added formation containing rows containing positions

---
+++ Assets/Scripts/PositionScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionScript : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+    }
+}
