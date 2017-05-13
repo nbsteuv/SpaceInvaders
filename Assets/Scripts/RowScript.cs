413c9097108912c7bb534b013347d2bf01c92af8
nbsteuv <nbsteuv@gmail.com>
2017-05-13 13:29:38 -0400

    Added enemy prefabs and instantiated to positions

---
+++ Assets/Scripts/RowScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowScript : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+
+	void Start () {
+        InstantiateEnemies();
+	}
+	
+	void Update () {
+		
+	}
+
+    void InstantiateEnemies()
+    {
+        foreach(Transform child in transform)
+        {
+            GameObject enemy = (GameObject)Instantiate(enemyPrefab, child.transform.position, Quaternion.identity);
+            enemy.transform.parent = child;
+        }
+    }
+}
