49b80415ba0c7ed3e08e032d8728ae61eabe0e5c
nbsteuv <nbsteuv@gmail.com>
2017-05-13 12:09:30 -0400

    Clamped player movement to screen width

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
 
     float cameraToBackgroundDistance = 10f;
+    float rightBoundary;
+    float leftBoundary;
 
 	void Start () {
-
-	}
+        rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, cameraToBackgroundDistance)).x;
+        leftBoundary = -rightBoundary;
+    }
 	
 	void Update () {
         float mousePositionX = Input.mousePosition.x / Screen.width;
         float newPositionX = Camera.main.ViewportToWorldPoint(new Vector3(mousePositionX, 0, cameraToBackgroundDistance)).x;
+        newPositionX = Mathf.Clamp(newPositionX, leftBoundary, rightBoundary);
         transform.position = new Vector3(newPositionX, transform.position.y, transform.position.z);
+
     }
 }
