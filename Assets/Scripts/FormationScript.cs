deb6e4d097f53a19d9c3ae1374e37a5333819ecd
nbsteuv <nbsteuv@gmail.com>
2017-05-13 14:58:51 -0400

    Formation script controls movement of rows for invaders-like movement

---
+++ Assets/Scripts/FormationScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormationScript : MonoBehaviour {
+
+    public float speed;
+    public float maxOffset;
+
+    List<RowScript> rowScripts;
+
+	void Start () {
+        rowScripts = new List<RowScript>();
+		foreach(Transform child in transform)
+        {
+            //Get and register row script
+            GameObject row = child.gameObject;
+            RowScript rowScript = row.GetComponent<RowScript>();
+            rowScripts.Add(rowScript);
+        }
+	}
+	
+	void Update () {
+        foreach(RowScript rowScript in rowScripts)
+        {
+            rowScript.SetSpeed(1);
+            rowScript.SetGoalPosition(10f);
+        }
+	}
+}
--- Assets/Scripts/RowScript.cs
+++ Assets/Scripts/RowScript.cs
@@ -6,12 +6,26 @@ public class RowScript : MonoBehaviour {
 
     public GameObject enemyPrefab;
 
+    float speed;
+    float movementStartTime;
+    float startX;
+    float goalX;
+    bool moving = false;
+
 	void Start () {
         InstantiateEnemies();
-	}
+        startX = transform.position.x;
+        //goalX = startX;
+    }
 	
 	void Update () {
-		
+        if (CheckMoveable())
+        {
+            Shift();
+        } else
+        {
+            moving = false;
+        }
 	}
 
     void InstantiateEnemies()
@@ -22,4 +36,49 @@ public class RowScript : MonoBehaviour {
             enemy.transform.parent = child;
         }
     }
+
+    public void SetSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+
+    public void SetGoalPosition(float newXPosition)
+    {
+        //Debug.Log("Setting position: " + newXPosition);
+        startX = transform.position.x;
+        goalX = newXPosition;
+    }
+
+    bool CheckMoveable()
+    {
+        //Debug.Log(goalX + " vs. " + startX);
+        if(goalX == startX)
+        {
+            return false;
+        }
+        //Debug.Log("Returning as moveable");
+        return true;
+    }
+
+    void Shift()
+    {
+        if(moving == false)
+        {
+            moving = true;
+            movementStartTime = Time.time;
+        }
+        float timeDifference = Time.time - movementStartTime;
+        float distanceTraveled = timeDifference * speed;
+
+        Vector3 startPosition = new Vector3(startX, transform.position.y, transform.position.z);
+        Vector3 goalPosition = new Vector3(goalX, transform.position.y, transform.position.z);
+        float totalDistance = Vector3.Distance(startPosition, goalPosition);
+
+        float fracJourney = distanceTraveled / totalDistance;
+
+        //Debug.Log(fracJourney);
+
+        transform.position = Vector3.Lerp(startPosition, goalPosition, fracJourney);
+    }
+    
 }
