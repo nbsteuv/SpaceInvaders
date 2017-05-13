30ab526fa6a3f82908782aca606dcc1c87a8692a
nbsteuv <nbsteuv@gmail.com>
2017-05-13 12:07:22 -0400

    Added player object with mouse control

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    float cameraToBackgroundDistance = 10f;
+
+	void Start () {
+
+	}
+	
+	void Update () {
+        float mousePositionX = Input.mousePosition.x / Screen.width;
+        float newPositionX = Camera.main.ViewportToWorldPoint(new Vector3(mousePositionX, 0, cameraToBackgroundDistance)).x;
+        transform.position = new Vector3(newPositionX, transform.position.y, transform.position.z);
+    }
+}
