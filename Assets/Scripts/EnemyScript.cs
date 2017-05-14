c690e86418dcf65711e4ebecdb330686a2b12d5b
nbsteuv <nbsteuv@gmail.com>
2017-05-14 09:03:03 -0400

    Enemies fire every 1 seconds

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -6,16 +6,18 @@ public class EnemyScript : MonoBehaviour {
 
     public GameObject laserPrefab;
     public float laserVerticalOffset;
+    public float maxCoolDown;
+
+    float coolDown = 1f;
+    float timer = 10f;
 
 	void Start () {
 		
 	}
 	
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Fire();
-        }
+        IncrementTimer();
+        FireIfReady();
 	}
 
     public void Die()
@@ -26,5 +28,32 @@ public class EnemyScript : MonoBehaviour {
     public void Fire()
     {
         Instantiate(laserPrefab, new Vector3(transform.position.x, transform.position.y + laserVerticalOffset, transform.position.z), Quaternion.identity);
+    }
+
+    bool CheckCooldown()
+    {
+        if(timer >= coolDown)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+
+    void FireIfReady()
+    {
+        if (CheckCooldown())
+        {
+            Fire();
+            timer = 0;
+        }
+    }
+
+    void IncrementTimer()
+    {
+        timer += Time.deltaTime;
     }
 }
