11b485496745e934076dacbb3585cb2425e2b0d7
nbsteuv <nbsteuv@gmail.com>
2017-05-14 08:33:13 -0400

    Enemy lasers have downward velocity

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour {
 
+    public GameObject laserPrefab;
+    public float laserVerticalOffset;
+
 	void Start () {
 		
 	}
 	
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Fire();
+        }
 	}
 
     public void Die()
     {
         Destroy(gameObject);
+    }
+
+    public void Fire()
+    {
+        Instantiate(laserPrefab, new Vector3(transform.position.x, transform.position.y + laserVerticalOffset, transform.position.z), Quaternion.identity);
     }
 }
