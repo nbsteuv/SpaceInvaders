a253474988973c0e38090047d191845525b1bee6
nbsteuv <nbsteuv@gmail.com>
2017-05-14 07:42:37 -0400

    Added velocity to blue lasers

---
+++ Assets/Scripts/LaserScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour {
+
+    public float speed;
+
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = new Vector3(0, speed, 0);
+	}
+	
+	void Update () {
+		
+	}
+}
