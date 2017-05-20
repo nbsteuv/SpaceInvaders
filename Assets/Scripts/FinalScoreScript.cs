df3e023915935d2385b6bd424024b15f031af7f2
nbsteuv <nbsteuv@gmail.com>
2017-05-19 21:04:32 -0400

    Final score script sets score according to what is passed to it

---
+++ Assets/Scripts/FinalScoreScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalScoreScript : MonoBehaviour {
+
+    public Text scoreDisplay;
+
+    int scoreValue;
+    int scoreCurrent;
+
+	void Update () {
+        MoveScore();
+	}
+
+    public void SetScore(int score)
+    {
+        scoreValue = score;
+    }
+
+    void MoveScore()
+    {
+        scoreCurrent = scoreValue;
+        scoreDisplay.text = scoreCurrent.ToString();
+    }
+}
