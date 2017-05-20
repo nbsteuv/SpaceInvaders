330b0f9cf26a578ab54532e68d0de32a9149d743
nbsteuv <nbsteuv@gmail.com>
2017-05-20 16:51:31 -0400

    Lose condition when enemies reach shredder

--- Assets/Scripts/ShredderScript.cs
+++ Assets/Scripts/ShredderScript.cs
@@ -6,6 +6,12 @@ public class ShredderScript : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            GameObject LevelManager = (GameObject)GameObject.Find("LevelManager");
+            LevelManagerScript levelManagerScript = LevelManager.GetComponent<LevelManagerScript>();
+            levelManagerScript.LoadLose();
+        }
         Destroy(collision.gameObject);
     }
 }
